4eda5884de8d3f0dcd3da35a1131022727aa0383
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-04-07 00:37:10 +0530

    Added grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,32 @@
 import Sidenav from "../components/Sidenav";
 import { Box } from "@mui/material";
 import Navbar from "../components/Navbar";
+import Grid from "@mui/material/Grid";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Box height={30} />
+      <Box height={70} />
       <Box sx={{ display: "flex" }}>
         <Sidenav />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <h1>Home</h1>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              Hello
+            </Grid>
+            <Grid item xs={4}>
+              Hello
+            </Grid>
+          </Grid>
+
+          <Box height={20} />
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              Hello
+            </Grid>
+            <Grid item xs={4}></Grid>
+          </Grid>
         </Box>
       </Box>
     </>
