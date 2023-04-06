7164b0e9f5efec72b77b10bc7e57e63df2b3d65b
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-04-06 19:15:33 +0530

    changes | 6 april

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Sidenav from "../components/Sidenav";
 import { Box } from "@mui/material";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Box sx={{ display: "flex" }}>
         <Sidenav />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
