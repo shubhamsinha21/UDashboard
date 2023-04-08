c8353dd3642c923f97dc43a3898a22dd6c93251f
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-04-08 15:12:56 +0530

    added dataTable

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,17 +1,17 @@
 import Sidenav from "../components/Sidenav";
 import { Box } from "@mui/material";
 import Navbar from "../components/Navbar";
-import ProductsList from "../charts/products/ProductsList";
+import ProductsList from "../products/ProductsList";
 
 export default function Products() {
   return (
     <>
       <Navbar />
-      <Box height={30} />
+      <Box height={70} />
       <Box sx={{ display: "flex" }}>
         <Sidenav />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <h1>Users</h1>
+          <ProductsList />
         </Box>
       </Box>
     </>
---
+++ src/products/EditProducts.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Editproducts() {
+  return <div>Editproducts</div>;
+}
